c00c169c930aec352c4d90cf0772946b1518eb43
enidsinclair <jazzg869@gmail.com>
2026-09-19 16:12:22 +0100

    Point website downloads to published Beta 4

--- docs/assets/site.js
+++ docs/assets/site.js
@@ -2,12 +2,12 @@
   const repository = 'videoediterNetVistaStudio.github.io';
   const repositoryURL = 'https://github.com/user1994g/videoediterNetVistaStudio.github.io';
   // Keep download links on a published release until the next assets exist.
-  const releaseTag = 'v1.4.0-beta.3';
+  const releaseTag = 'v1.4.0-beta.4';
   const releaseURL = `${repositoryURL}/releases/tag/${releaseTag}`;
   const downloads = {
-    mac: `${repositoryURL}/releases/download/${releaseTag}/NetVista-Studio-macOS-1.4-Beta-3.zip`,
-    windows: `${repositoryURL}/releases/download/${releaseTag}/NetVista-Studio-Windows-1.4-Beta-3.zip`,
-    linux: `${repositoryURL}/releases/download/${releaseTag}/NetVista-Studio-Linux-1.4-Beta-3.zip`
+    mac: `${repositoryURL}/releases/download/${releaseTag}/NetVista-Studio-macOS-1.4-Beta-4.zip`,
+    windows: `${repositoryURL}/releases/download/${releaseTag}/NetVista-Studio-Windows-1.4-Beta-4.zip`,
+    linux: `${repositoryURL}/releases/download/${releaseTag}/NetVista-Studio-Linux-1.4-Beta-4.zip`
   };
 
   document.querySelectorAll('.github-link').forEach((link) => { link.href = repositoryURL; });
